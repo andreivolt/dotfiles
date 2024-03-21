268e2da5c172f7bbe38cc9734bb25f6ecc40c4a4
Andrei Volt <andrei@avolt.net>
2024-03-22 00:31:55 +0100

    update

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,3 +1,6 @@
+hs.loadSpoon("SpoonInstall")
+spoon.SpoonInstall:updateAllRepos()
+
 local ipc = require("hs.ipc")
 if not ipc.cliStatus("/usr/local") then
   ipc.cliInstall()
@@ -6,24 +9,21 @@ end
 local screen = require("hs.screen")
 local application = require("hs.application")
 
-local function resizeKittyWindow()
-    local kittyApp = hs.application.get("kitty")
-    hs.console.printStyledtext("hey")
-    if kittyApp then
-        local kittyPID = kittyApp:pid()
-        local kittyPath = "/Applications/kitty.app/Contents/MacOS/kitty"
-        local sockPath = string.format("/tmp/kitty-%d", kittyPID)
+-- local function resizeKittyWindow()
+--     local kittyApp = hs.application.get("kitty")
+--     if kittyApp then
+--         local kittyPID = kittyApp:pid()
+--         local kittyPath = "/Applications/kitty.app/Contents/MacOS/kitty"
+--         local sockPath = string.format("/tmp/kitty-%d", kittyPID)
 
-        local cmd = string.format("%s @ --to=unix:%s resize-os-window --match all --action toggle-fullscreen", kittyPath, sockPath)
+--         local cmd = string.format("%s @ --to=unix:%s resize-os-window --match all --action toggle-fullscreen", kittyPath, sockPath)
 
-        hs.console.printStyledtext(cmd)
-        hs.execute(cmd)
-        hs.execute(cmd)
-    end
-end
-
-screen.watcher.newWithActiveScreen(resizeKittyWindow):start()
-hs.console.printStyledtext("Screen watcher started")
+--         hs.console.printStyledtext(cmd)
+--         hs.execute(cmd)
+--         hs.execute(cmd)
+--     end
+-- end
+-- screen.watcher.newWithActiveScreen(resizeKittyWindow):start()
 
 hs.hotkey.bind({}, "²", function()
   local app = hs.application.get("kitty")
@@ -39,11 +39,14 @@ hs.hotkey.bind({}, "²", function()
     hs.application.launchOrFocus("kitty")
     app = hs.application.get("kitty")
   end
-  hs.console.printStyledtext("Kitty hotkey triggered")
 end)
 
-hs.hotkey.bind({"ctrl", "alt"}, "R", function()
+hs.hotkey.bind({"ctrl", "cmd"}, "r", function()
   hs.execute("/Users/andrei/drive/bin/randomtab", true)
+end)
+
+hs.hotkey.bind({"ctrl", "cmd"}, "v", function()
+  hs.execute("/Users/andrei/drive/bin/vision -c", true)
 end)
 
 
@@ -54,7 +57,7 @@ end
 hs.hotkey.bind({"ctrl", "cmd"}, "1", function() activateApp("Google Chrome") end)
 hs.hotkey.bind({"ctrl", "cmd"}, "2", function() activateApp("Kitty") end)
 hs.hotkey.bind({"ctrl", "cmd"}, "3", function() activateApp("Spotify") end)
-hs.hotkey.bind({"ctrl", "cmd"}, "4", function() activateApp("Mimestream") end)
+hs.hotkey.bind({"ctrl", "cmd"}, "4", function() activateApp("Sublime Text") end)
 
 hs.hotkey.bind({"ctrl", "cmd"}, "d", function()
   hs.osascript.applescript([[
@@ -65,3 +68,41 @@ hs.hotkey.bind({"ctrl", "cmd"}, "d", function()
     end tell
   ]])
 end)
+
+spoon.SpoonInstall:andUse("ReloadConfiguration", {
+    repo = "default", -- Repository where the spoon is located
+    watch_paths = {
+        hs.configdir, -- Default Hammerspoon config directory
+    }
+})
+spoon.ReloadConfiguration:start()
+
+spoon.SpoonInstall:andUse("WinWin", {
+    repo = "default" -- Repository where WinWin is located
+})
+
+require("finder")
+require("terminal")
+
+local function isWhatsAppOrTelegram(window)
+    local app = window:application()
+    return app:name() == "WhatsApp" or app:name() == "Telegram"
+end
+
+local function resizeAndCenterWindow(window)
+    local screen = window:screen()
+    local screenFrame = screen:frame()
+    local windowFrame = {
+        x = screenFrame.x + (screenFrame.w * 0.125),
+        y = screenFrame.y + (screenFrame.h * 0.125),
+        w = screenFrame.w * 0.75,
+        h = screenFrame.h * 0.75
+    }
+end
+
+local windowFilter = hs.window.filter.new(true)
+    :subscribe(hs.window.filter.windowCreated, function(window)
+        if isWhatsAppOrTelegram(window) then
+            resizeAndCenterWindow(window)
+        end
+    end)
