00d28bfe508e1c841049c3033f4f6ca7a6b5a0d9
Andrei Volt <andrei@avolt.net>
2024-03-27 12:48:16 +0100

    update

--- .zsh.d/macos.zsh
+++
@@ -1,6 +0,0 @@
-# macos setsid
-[ ! -f /usr/local/opt/util-linux/bin/setsid ] && which brew >/dev/null && brew install util-linux
-alias setsid=/usr/local/opt/util-linux/bin/setsid
-
-# macos gnu find
-PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
