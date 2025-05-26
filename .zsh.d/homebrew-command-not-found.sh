ca0b013a3d4748f9bdefdc59541bade3fe688f33
Andrei Volt <andrei@avolt.net>
2025-05-26 10:32:40 +0200

    update

--- .zsh.d/homebrew-command-not-found.sh
+++
@@ -1,4 +0,0 @@
-HB_CNF_HANDLER=/opt/homebrew/Library/Taps/homebrew/homebrew-command-not-found/handler.sh
-if [ -f "$HB_CNF_HANDLER" ]; then
-  source "$HB_CNF_HANDLER"
-fi
--- .zsh.d/kitty.zsh
+++ .zsh.d/kitty.zsh
@@ -1,8 +1,6 @@
-if test -n "$KITTY_INSTALLATION_DIR"; then
-  export KITTY_SHELL_INTEGRATION="enabled"
-  autoload -Uz -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty-integration
-  kitty-integration
-  unfunction kitty-integration
-fi
+export KITTY_SHELL_INTEGRATION="enabled"
+autoload -Uz -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty-integration
+kitty-integration
+unfunction kitty-integration
 
 alias ssh='kitty +kitten ssh'
