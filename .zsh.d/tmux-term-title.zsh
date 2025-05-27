90599ad84c715e722d26b8bb00b67e695d2e9908
Andrei Volt <andrei@avolt.net>
2025-05-27 23:29:13 +0200

    update

--- .zsh.d/tmux-term-title.zsh
+++ .zsh.d/tmux-term-title.zsh
@@ -1,10 +1,10 @@
-# Set tmux window title to current command
+# set tmux window title to current command
 preexec() {
   local cmd=${1%% *}
   printf "\033k$cmd\033\\"
 }
 
-# Reset tmux window title when command finishes
+# reset tmux window title when command finishes
 precmd() {
   printf "\033kzsh\033\\"
 }
