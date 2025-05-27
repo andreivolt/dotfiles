24d93dc8b156d3cdd943e8c6a819553f11c467e0
Andrei Volt <andrei@avolt.net>
2025-05-27 23:29:13 +0200

    update

--- .zsh.d/aliases.sh
+++ .zsh.d/aliases.sh
@@ -1,11 +1,11 @@
 alias eza="eza --icons never"
+alias ls="eza"
 alias l="ls -1"
 alias la="ls -a"
 alias ll="ls -l --classify=auto --git"
 alias lla="ll -a"
-alias ls="eza"
 
-alias cal="cal -m"
+alias diff="diff --color"
 alias gc='git clone --depth 1'
 alias gist="gh gist"
 alias gron="fastgron"
@@ -13,6 +13,6 @@ alias j="jobs"
 alias rg='rg --smart-case --colors match:bg:yellow --colors match:fg:black'
 alias rm="rm --verbose"
 alias vi="nvim"
-alias yt-dlp="yt-dlp --cookies-from-browser firefox"
+alias yt-dlp="yt-dlp --cookies-from-browser chrome"
 
 alias tailscale="/Applications/Tailscale.localized/Tailscale.app/Contents/MacOS/Tailscale"
