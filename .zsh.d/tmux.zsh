94d8f647b11ef8ddcb87204fe404c4ad4ba9e6aa
Andrei Volt <andrei@avolt.net>
2025-06-20 03:14:06 +0200

    update

--- .zsh.d/tmux.zsh
+++ .zsh.d/tmux.zsh
@@ -1,14 +1,11 @@
-# set tmux window title to current command
 preexec() {
   local cmd=${1%% *}
   printf "\033k$cmd\033\\"
 }
 
-# reset tmux window title when command finishes
 precmd() {
   printf "\033kzsh\033\\"
 }
 
-# Add to hook arrays
 preexec_functions+=(preexec)
 precmd_functions+=(precmd)
