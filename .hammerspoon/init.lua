3d8a2b98851c9f3805a215c5c18e236d4d2af9eb
Andrei Volt <andrei@avolt.net>
2025-06-30 22:07:09 +0200

    update

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,61 +1,44 @@
 require("hs.ipc")
 
-hs.loadSpoon("SpoonInstall")
-
-spoon.SpoonInstall:updateAllRepos()
-
-local screen = require("hs.screen")
-local application = require("hs.application")
-
 local toggleApp = require("toggle_app")
+
 require("hotkeys")
 
-spoon.SpoonInstall:andUse("ReloadConfiguration", {
-  repo = "default", -- Repository where the spoon is located
-  watch_paths = {
-    hs.configdir, -- Default Hammerspoon config directory
-  },
-})
+hs.loadSpoon("SpoonInstall")
+spoon.SpoonInstall:updateAllRepos()
 
+spoon.SpoonInstall:andUse("ReloadConfiguration", {
+  repo = "default",
+  watch_paths = { hs.configdir },
+})
 spoon.ReloadConfiguration:start()
 
 spoon.SpoonInstall:andUse("WinWin", {
-  repo = "default", -- Repository where WinWin is located
+  repo = "default",
 })
 
-local redshift = require("redshift")
-redshift.init()
-
--- Function to handle Ghostty resizing
 local function handleGhosttyResize()
   local ghostty = hs.application.get("ghostty")
-  
-  if ghostty then
-    local mainWindow = ghostty:mainWindow()
-    
-    if mainWindow then
-      local currentScreen = mainWindow:screen()
-      
-      if #hs.screen.allScreens() > 1 then
-        toggleApp.toggleAppMonitor()
-      else
-        mainWindow:maximize()
-      end
-    end
+  if not ghostty then return end
+
+  local mainWindow = ghostty:mainWindow()
+  if not mainWindow then return end
+
+  if #hs.screen.allScreens() > 1 then
+    toggleApp.toggleAppMonitor()
+  else
+    mainWindow:maximize()
   end
 end
 
--- Auto-move Ghostty to external monitor when connected
 hs.screen.watcher.new(function()
   hs.timer.doAfter(0.5, handleGhosttyResize)
 end):start()
 
--- Also watch for resolution changes specifically
 hs.spaces.watcher.new(function()
   hs.timer.doAfter(0.5, handleGhosttyResize)
 end):start()
 
--- CLI setup
 hs.ipc.cliInstall()
 
 if hs.ipc then
