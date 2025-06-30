935b72767b6448c8e837a18d4bd20958ef468497
Andrei Volt <andrei@avolt.net>
2025-06-30 15:52:15 +0200

    update

--- .zshrc.extra.zsh
+++ .zshrc.extra.zsh
@@ -2,13 +2,13 @@ HISTSIZE="999999"
 SAVEHIST="999999"
 HISTFILE="${XDG_STATE_HOME:-$HOME/.local/state}/zsh/history"
 mkdir -p ${HISTFILE:h}
+setopt append_history
+setopt extended_history
+setopt share_history
 setopt hist_fcntl_lock
 setopt hist_ignore_dups
 setopt hist_ignore_space
 setopt hist_reduce_blanks
-setopt share_history
-setopt extended_history
-setopt append_history
 
 setopt auto_cd
 setopt auto_pushd
@@ -24,8 +24,6 @@ alias -g H='| head'
 alias -g L="| $PAGER"
 alias -g NUL="&> /dev/null"
 alias -g X='| xargs'
-alias -g COPY='| pbcopy'
-alias -g PASTE='pbpaste |'
 
 alias eza="eza --icons never"
 alias ls="eza"
@@ -43,7 +41,6 @@ alias gron="fastgron"
 alias hgrep='history | grep -i --color'
 alias j="jobs"
 alias path='printf "%s\n" $path'
-alias reload-history='fc -p && fc -R'
 alias rg='rg --smart-case --colors match:bg:yellow --colors match:fg:black'
 alias rm="rm --verbose"
 alias vi="nvim"
@@ -59,20 +56,21 @@ source ~/.local/share/zsh/plugins/zsh-defer/zsh-defer.plugin.zsh
 source ~/.zsh.d/vi.zsh
 
 source ~/.zsh.d/prompt.zsh
+source ~/.zsh.d/completion.zsh
 
 # source ~/.zsh.d/accept-line.zsh
 # zsh-defer source ~/.local/share/zsh/plugins/zummoner/zummoner.plugin.zsh
-(( $+commands[orbctl] )) && zsh-defer source ~/.zsh.d/orbstack.zsh
+
+[[ "$OSTYPE" == darwin* ]] && zsh-defer source ~/.zsh.d/orbstack.zsh
 [[ $TERM == xterm-kitty ]] && zsh-defer source ~/.zsh.d/kitty.zsh
 [[ -n "$TMUX" ]] && zsh-defer source ~/.zsh.d/tmux.zsh
 zsh-defer eval "$(dircolors -b ~/.dircolors)"
 zsh-defer source ~/.local/share/zsh/plugins/nix-shell/nix-shell.plugin.zsh
-zsh-defer source ~/.zsh.d/history-search/history-search.zsh
 zsh-defer source ~/.zsh.d/autopair.zsh
 zsh-defer source ~/.zsh.d/autosuggestions.zsh
-zsh-defer source ~/.zsh.d/completion.zsh
 zsh-defer source ~/.zsh.d/direnv.zsh
 zsh-defer source ~/.zsh.d/fzf.zsh
+zsh-defer source ~/.zsh.d/history-search/history-search.zsh
 zsh-defer source ~/.zsh.d/history-substring-search.zsh
 
 zsh-defer source ~/.local/share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
