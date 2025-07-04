c140463653cc61c8aec39401d5bba047869e7f9f
Andrei Volt <andrei@avolt.net>
2025-07-04 11:38:36 +0200

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,58 +1,58 @@
 local wezterm = require 'wezterm'
-local config = wezterm.config_builder()
 
-config.font = wezterm.font_with_fallback({
-  {
-    family = "IosevkaTerm Nerd Font Mono",
-    weight = "Light",
+local config = {
+  font = wezterm.font_with_fallback({
+    {
+      family = "IosevkaTerm Nerd Font Mono",
+      weight = "Light",
+    },
+    "Noto Color Emoji",
+  }),
+  font_rules = {
+    {
+      italic = true,
+      font = wezterm.font_with_fallback({
+        {
+          family = "IosevkaTerm Nerd Font Mono",
+          weight = "Light",
+          style = "Italic",
+        },
+      }),
+    },
+    {
+      intensity = "Bold",
+      font = wezterm.font_with_fallback({
+        {
+          family = "IosevkaTerm Nerd Font Mono",
+          weight = "Regular",
+        },
+      }),
+    },
+    {
+      italic = true,
+      intensity = "Bold",
+      font = wezterm.font_with_fallback({
+        {
+          family = "IosevkaTerm Nerd Font Mono",
+          weight = "Regular",
+          style = "Italic",
+        },
+      }),
+    },
   },
-  "Noto Color Emoji",
-})
-config.font_rules = {
-  {
-    italic = true,
-    font = wezterm.font_with_fallback({
-      {
-        family = "IosevkaTerm Nerd Font Mono",
-        weight = "Light",
-        style = "Italic",
-      },
-    }),
+  enable_tab_bar = false,
+  cell_width = 0.83,
+  line_height = 0.9,
+  window_decorations = "NONE",
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
   },
-  {
-    intensity = "Bold",
-    font = wezterm.font_with_fallback({
-      {
-        family = "IosevkaTerm Nerd Font Mono",
-        weight = "Regular",
-      },
-    }),
-  },
-  {
-    italic = true,
-    intensity = "Bold",
-    font = wezterm.font_with_fallback({
-      {
-        family = "IosevkaTerm Nerd Font Mono",
-        weight = "Regular",
-        style = "Italic",
-      },
-    }),
-  },
+  -- disable_default_key_bindings = true,
+  window_background_opacity = 0.85,
+  font_size = 18,
 }
-
-config.enable_tab_bar = false
-config.cell_width = 0.83
-config.line_height = 0.9
-config.window_decorations = "NONE"
-config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
-}
--- config.disable_default_key_bindings = true
-config.window_background_opacity = 0.85
-config.font_size = 18
 
 return config
