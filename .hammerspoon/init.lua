2afcb494937476d5e86d129c7680470fbb815d36
Andrei Volt <andrei@avolt.net>
2025-07-04 19:58:32 +0200

    update

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,6 +1,8 @@
 require("hs.ipc")
 
 local ghostty = require("ghostty")
+local darkmode = require("darkmode")
+local ghosttyResize = require("ghostty-resize")
 
 hs.loadSpoon("SpoonInstall")
 spoon.SpoonInstall:updateAllRepos()
@@ -11,48 +13,14 @@ spoon.SpoonInstall:andUse("ReloadConfiguration", {
 })
 spoon.ReloadConfiguration:start()
 
-local function toggleDarkMode()
-	hs.osascript.applescript([[
-        tell application "System Events"
-            tell appearance preferences
-                set dark mode to not dark mode
-            end tell
-        end tell
-    ]])
-end
 
-hs.hotkey.bind({}, "²", function()
-	ghostty.toggleVisibility()
-end)
-
-hs.hotkey.bind({ "alt" }, "²", function()
-	ghostty.toggleMonitor()
-end)
-
+hs.hotkey.bind({}, "²", ghostty.toggleVisibility)
+hs.hotkey.bind({ "alt" }, "²", ghostty.toggleMonitor)
 hs.hotkey.bind({ "ctrl", "cmd" }, "v", function()
-	hs.execute("/Users/andrei/bin/vision -c", true)
+  hs.execute("/Users/andrei/bin/vision -c", true)
 end)
+hs.hotkey.bind({ "ctrl", "cmd" }, "d", darkmode.toggleDarkMode)
 
-hs.hotkey.bind({ "ctrl", "cmd" }, "d", function()
-	toggleDarkMode()
-end)
-
-local function handleGhosttyResize()
-  local ghosttyApp = hs.application.get("ghostty")
-  if not ghosttyApp then return end
-
-  local mainWindow = ghosttyApp:mainWindow()
-  if not mainWindow then return end
-
-  if #hs.screen.allScreens() > 1 then
-    ghostty.toggleMonitor()
-  else
-    mainWindow:maximize()
-  end
-end
-
-hs.screen.watcher.new(function()
-  hs.timer.doAfter(0.5, handleGhosttyResize)
-end):start()
+ghosttyResize.start()
 
 hs.ipc.cliInstall()
