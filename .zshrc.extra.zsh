40f92390c470d906ec911089f1de67025e39776b
Andrei Volt <andrei@avolt.net>
2025-07-12 06:59:09 +0200

    update

--- .zshrc.extra.zsh
+++ .zshrc.extra.zsh
@@ -156,35 +156,6 @@ source ~/.local/share/zsh/plugins/powerlevel10k/powerlevel10k.zsh-theme
 # source ~/.zsh.d/accept-line.zsh
 # zsh-defer source ~/.local/share/zsh/plugins/zummoner/zummoner.plugin.zsh
 
-# <tab> at beginning of line opens fzf file selector
-function fzf-file-widget-open() {
-  if [[ -z "$BUFFER" ]]; then
-    local selected=$(
-      rg --hidden --follow --files --sort modified --follow 2>/dev/null | tac |
-	fzf --preview 'if head -c 1024 {} | file - | grep -q "text"; then bat --color=always --style=numbers --line-range=:500 {}; else (2text {} || mediainfo {}) 2>/dev/null || echo "Cannot preview file"; fi')
-    if [[ -n "$selected" ]]; then
-      # Check if file is text (same logic as preview)
-      if ! head -c 1024 "$selected" | file - | grep -q "text"; then
-	# Binary file - use system opener
-	if [[ -n "$TERMUX_VERSION" ]]; then
-	  BUFFER="termux-open '$selected'"
-	else
-	  BUFFER="open '$selected'"
-	fi
-      else
-	# Text file that bat can handle - use nvim
-	BUFFER="nvim '$selected'"
-      fi
-      zle accept-line
-    fi
-  else
-    zle expand-or-complete
-  fi
-}
-zle -N fzf-file-widget-open
-bindkey '^I' fzf-file-widget-open
-bindkey -M vicmd '^I' fzf-file-widget-open
-
 [[ "$OSTYPE" == darwin* ]] && zsh-defer source ~/.zsh.d/orbstack.zsh
 [[ $TERM == xterm-kitty ]] && zsh-defer source ~/.zsh.d/kitty.zsh
 [[ -n "$TMUX" ]] && zsh-defer source ~/.zsh.d/tmux.zsh
