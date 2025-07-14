3baab97258e8d5cffbe51b111444138c73be1176
Andrei Volt <andrei@avolt.net>
2025-07-14 14:40:20 +0200

    update

--- .zshrc.extra.zsh
+++ .zshrc.extra.zsh
@@ -144,7 +144,7 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
 	source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 source ~/.local/share/zsh/plugins/powerlevel10k/powerlevel10k.zsh-theme
-[[ -f ~/.zsh.d/p10k.zsh ]] && source ~/.zsh.d/p10k.zsh
+source ~/.zsh.d/p10k.zsh
 
 [[ "$OSTYPE" == darwin* ]] && zsh-defer source ~/.zsh.d/orbstack.zsh
 [[ $TERM == xterm-kitty ]] && zsh-defer source ~/.zsh.d/kitty.zsh
