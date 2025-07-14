95f73f26728018bbf43bdfbb388741002e39ff6e
Andrei Volt <andrei@avolt.net>
2025-07-14 14:47:29 +0200

    update

--- .zshrc.extra.zsh
+++ .zshrc.extra.zsh
@@ -59,7 +59,7 @@ zcompdump=$XDG_CACHE_HOME/zsh/zcompdump
 [[ -f $zcompdump(#qN.mh+24) ]] && { compinit -d $zcompdump && zcompile $zcompdump } || compinit -C -d $zcompdump
 
 (( ${+commands[brew]} )) && fpath=(/opt/homebrew/share/zsh/site-functions $fpath)
-
+""
 setopt no_list_ambiguous
 setopt glob_complete
 setopt complete_in_word
@@ -71,7 +71,7 @@ bindkey -M menuselect '^o' accept-and-menu-complete
 bindkey -M menuselect "+" accept-and-menu-complete
 
 zstyle ':completion:*' use-cache on
-zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
+zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/zcompcache"
 
 zstyle ':completion:*' accept-exact '*(N)'
 zstyle ':completion:*' rehash true
@@ -106,31 +106,31 @@ zstyle -e ':completion:*' completer '
 			reply=(_complete _expand_alias _prefix)
 			;;
 	esac
-	'
+'
 
-	zstyle ':completion:*:approximate:*' max-errors '(( reply=($#PREFIX+$#SUFFIX)/3 ))'
-	zstyle -e ':completion:*:approximate-extreme:*' max-errors '(( reply=($#PREFIX+$#SUFFIX)/1.2 ))'
-	zstyle ':completion:*:(correct|approximate[^:]#):*' original false
-	zstyle ':completion:*:(correct|approximate[^:]#):*' tag-order '! original'
+zstyle ':completion:*:approximate:*' max-errors '(( reply=($#PREFIX+$#SUFFIX)/3 ))'
+zstyle -e ':completion:*:approximate-extreme:*' max-errors '(( reply=($#PREFIX+$#SUFFIX)/1.2 ))'
+zstyle ':completion:*:(correct|approximate[^:]#):*' original false
+zstyle ':completion:*:(correct|approximate[^:]#):*' tag-order '! original'
 
-	zstyle ':completion::(^approximate*):*:functions' ignored-patterns '_*'
-	zstyle ':completion::*:(bindkey|zle):*:widgets' ignored-patterns '.*'
-	zstyle ':completion::*:(mv|cp|rm|chmod|chown|vi):*' ignore-line true
-	zstyle ':completion::*:(scp|rsync):*' list-colors "=(#b)(*)/=0="${${${(s.:.)LS_COLORS}[(r)di=<->]}/di=/} '='${^${(M)${(s.:.)LS_COLORS}:#\**}}
+zstyle ':completion::(^approximate*):*:functions' ignored-patterns '_*'
+zstyle ':completion::*:(bindkey|zle):*:widgets' ignored-patterns '.*'
+zstyle ':completion::*:(mv|cp|rm|chmod|chown|vi):*' ignore-line true
+zstyle ':completion::*:(scp|rsync):*' list-colors "=(#b)(*)/=0="${${${(s.:.)LS_COLORS}[(r)di=<->]}/di=/} '='${^${(M)${(s.:.)LS_COLORS}:#\**}}
 
-		zstyle ':completion:*:man:*' menu yes select
-		zstyle ':completion:*:manuals' separate-sections true
-		zstyle ':completion:*:manuals.*' insert-sections true
+zstyle ':completion:*:man:*' menu yes select
+zstyle ':completion:*:manuals' separate-sections true
+zstyle ':completion:*:manuals.*' insert-sections true
 
-		zstyle ':completion:*:processes' command 'ps -au $USER'
-		zstyle ':completion:*:processes-names' command 'ps c -u ${USER} -o command | uniq'
+zstyle ':completion:*:processes' command 'ps -au $USER'
+zstyle ':completion:*:processes-names' command 'ps c -u ${USER} -o command | uniq'
 
-		zstyle ':completion:*:expand:*' tag-order all-expansions
-		zstyle ':completion:*:expand-alias:*' global true
-		zstyle ':completion:*:history-words' list false
+zstyle ':completion:*:expand:*' tag-order all-expansions
+zstyle ':completion:*:expand-alias:*' global true
+zstyle ':completion:*:history-words' list false
 
-		zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
-			reply=(_approximate:-extreme _complete)
+zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
+	reply=(_approximate:-extreme _complete)
 
 # history expansion
 bindkey ' ' magic-space
