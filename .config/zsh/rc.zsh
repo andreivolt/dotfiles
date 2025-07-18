415aab4e1f13afada27212782fefae30386beafd
Andrei Volt <andrei@avolt.net>
2025-07-18 11:32:47 +0200

    zsh

--- .config/zsh/rc.zsh
+++ .config/zsh/rc.zsh
@@ -17,22 +17,22 @@ setopt numeric_glob_sort
 
 READNULLCMD=$PAGER
 
-alias -g C='| wc -l'
+alias -g C="| wc -l"
 alias -g G="| rg"
-alias -g H='| head'
+alias -g H="| head"
 alias -g L="| $PAGER"
 alias -g N="&> /dev/null"
 alias -g NE="2> /dev/null"
-alias -g X='| xargs'
+alias -g X="| xargs"
 
-alias -- +x='chmod +x'
+alias -- +x="chmod +x"
 alias cat="bat"
-alias cdt='cd $(mktemp -d)'
+alias cdt="cd $(mktemp -d)"
 alias claude="claude --dangerously-skip-permissions"
 alias diff="diff --color"
 alias edir="edir -r"
 alias eza="eza --icons never"
-alias gc='git clone --depth 1'
+alias gc="git clone --depth 1"
 alias gron="fastgron"
 alias http="xh"
 alias jq="gojq"
@@ -42,91 +42,18 @@ alias ll="ls -l --classify=auto --git"
 alias lla="ll -a"
 alias ls="eza"
 alias path='printf "%s\n" $path'
-alias rg='rg --smart-case --colors match:bg:yellow --colors match:fg:black'
+alias rg="rg --smart-case --colors match:bg:yellow --colors match:fg:black"
 alias rm="rm --verbose"
 alias scrcpy="scrcpy --render-driver opengl"
 alias vi="nvim"
 alias yt-dlp="yt-dlp --cookies-from-browser chrome"
 
-source ~/.config/zsh/vi.zsh
-
-autoload -Uz compinit
-zcompdump=$XDG_CACHE_HOME/zsh/zcompdump
-[[ -f $zcompdump(#qN.mh+24) ]] && compinit -d $zcompdump && zcompile $zcompdump || compinit -C -d $zcompdump
-
-(( ${+commands[brew]} )) && fpath=(/opt/homebrew/share/zsh/site-functions $fpath)
-
 bindkey ' ' magic-space # history expansion
 
-bindkey -M menuselect '^o' accept-and-menu-complete
-bindkey -M menuselect "+" accept-and-menu-complete
-
-setopt no_list_ambiguous
-setopt glob_complete
-setopt complete_in_word
-setopt list_packed
-
-_comp_options+=(globdots)
-
-zstyle ':completion:*' use-cache on
-zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/zcompcache"
-
-zstyle ':completion:*' accept-exact '*(N)'
-zstyle ':completion:*' auto-description 'specify: %d'
-zstyle ':completion:*' file-patterns '%p:globbed-files' '*(-/):directories'
-zstyle ':completion:*' format 'Completing %d'
-zstyle ':completion:*' group-name ''
-zstyle ':completion:*' insert-unambiguous true
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-zstyle ':completion:*' menu select
-zstyle ':completion:*' rehash true
-zstyle ':completion:*' squeeze-slashes yes
-zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS} 'ma=30;46'
-zstyle ':completion:*:default' select-prompt '%SMatch %M Line %L %P%s'
-zstyle ':completion:*:matches' group 'yes'
-zstyle ':completion:*:messages' format '%d'
-zstyle ':completion:*:options' auto-description '%d'
-zstyle ':completion:*:options' description 'yes'
-zstyle ':completion:*:warnings' format "%B$fg[red]%}No matches for: $fg[white]%d%b"
-
-zstyle -e ':completion:*' completer '
-	case $_last_try in
-		$HISTNO$BUFFER$CURSOR)
-			reply=(_ignored _approximate _complete)
-			_last_try="$HISTNO$BUFFER${CURSOR}x"
-			;;
-		$HISTNO$BUFFER${CURSOR}x)
-			;;
-		*)
-			_last_try="$HISTNO$BUFFER$CURSOR"
-			reply=(_complete _expand_alias _prefix)
-			;;
-	esac
-'
-
-zstyle ':completion:*:approximate:*' max-errors '(( reply=($#PREFIX+$#SUFFIX)/3 ))'
-zstyle -e ':completion:*:approximate-extreme:*' max-errors '(( reply=($#PREFIX+$#SUFFIX)/1.2 ))'
-zstyle ':completion:*:(correct|approximate[^:]#):*' original false
-zstyle ':completion:*:(correct|approximate[^:]#):*' tag-order '! original'
-
-zstyle ':completion::(^approximate*):*:functions' ignored-patterns '_*'
-zstyle ':completion::*:(bindkey|zle):*:widgets' ignored-patterns '.*'
-
-zstyle ':completion:*:man:*' menu yes select
-zstyle ':completion:*:manuals' separate-sections true
-zstyle ':completion:*:manuals.*' insert-sections true
-
-zstyle ':completion:*:processes' command 'ps -au $USER'
-zstyle ':completion:*:processes-names' command 'ps c -u ${USER} -o command | uniq'
-
-zstyle ':completion:*:expand:*' tag-order all-expansions
-zstyle ':completion:*:expand-alias:*' global true
-zstyle ':completion:*:history-words' list false
-
-zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
-	reply=(_approximate:-extreme _complete)
-
 source ~/.local/share/zsh/plugins/zsh-defer/zsh-defer.plugin.zsh
+
+source ~/.config/zsh/vi.zsh
+source ~/.config/zsh/completion.zsh
 
 source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 source ~/.local/share/zsh/plugins/powerlevel10k/powerlevel10k.zsh-theme
@@ -135,7 +62,6 @@ source ~/.config/zsh/p10k.zsh
 [[ "$OSTYPE" == darwin* ]] && zsh-defer source ~/.config/zsh/orbstack.zsh
 [[ $TERM == xterm-kitty ]] && zsh-defer source ~/.config/zsh/kitty.zsh
 [[ -n "$TMUX" ]] && zsh-defer source ~/.config/zsh/tmux.zsh
-zsh-defer eval "$(carapace _carapace zsh)"
 zsh-defer eval "$(dircolors -b ~/.config/dircolors)"
 zsh-defer source ~/.local/share/zsh/plugins/nix-shell/nix-shell.plugin.zsh
 zsh-defer source ~/.config/zsh/autopair.zsh
