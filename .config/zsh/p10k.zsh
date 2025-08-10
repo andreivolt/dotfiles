217a2668c1d33d4c5c75e2a7efc33a780f20ba13
Andrei Volt <andrei@avolt.net>
2025-08-10 20:41:15 +0200

    bg

--- .config/zsh/p10k.zsh
+++ .config/zsh/p10k.zsh
@@ -437,6 +437,8 @@
   #######################[ background_jobs: presence of background jobs ]#######################
   # Don't show the number of background jobs.
   typeset -g POWERLEVEL9K_BACKGROUND_JOBS_VERBOSE=true
+  # Show job count even when there's only 1 job.
+  typeset -g POWERLEVEL9K_BACKGROUND_JOBS_VERBOSE_ALWAYS=true
   # Background jobs color.
   typeset -g POWERLEVEL9K_BACKGROUND_JOBS_FOREGROUND=70
   # Custom icon.
